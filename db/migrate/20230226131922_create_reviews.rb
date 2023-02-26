e4892913c4befb7f12262e59c4bad650acdcd947
Renee Mumbi <1723renee@gmail.com>
2023-02-26 19:27:51 +0300

    Modify attributes for reviews

--- db/migrate/20230226131922_create_reviews.rb
+++ db/migrate/20230226131922_create_reviews.rb
@@ -1,11 +1,13 @@
 class CreateReviews < ActiveRecord::Migration[6.1]
   def change
-      create_table :reviews do |t|
-        t.integer :star_rating
-        t.string :comment
-        t.belongs_to :user, null: false, foreign_key: true
-        t.belongs_to :products, null: false, foreign_key: true
-        
-      end
+    create_table :reviews do |t|
+      t.references :product, foreign_key: true
+      t.references :user, foreign_key: true
+      t.integer :star_rating
+      t.string :comment
+    
+      t.timestamps
+    end
+    
   end
 end
