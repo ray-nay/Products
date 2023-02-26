d5f2d061a8ec43ba16defe9405d36a23e4cf79c4
Renee Mumbi <1723renee@gmail.com>
2023-02-26 19:23:03 +0300

    Create product methods for handling reviews

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,20 @@
 class Product < ActiveRecord::Base
+    # attr_accessor :name, :desc
+
     has_many :reviews
     has_many :users, through: :reviews
+    
+    def leave_review(user, star_rating, comment)
+        Review.create!(user: user, product: self, star_rating: star_rating, comment: comment)
+      end
+    
+      def print_all_reviews
+        reviews.each do |review|
+          review.print_review
+        end
+      end
+    
+      def average_rating
+        reviews.average(:star_rating).to_f
+      end
 end 
