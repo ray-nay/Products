73bc4135e4235256d546ad78509146ddaf2f608f
Renee Mumbi <1723renee@gmail.com>
2023-02-26 19:25:13 +0300

    Create favorite product & remove reviews methods.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ActiveRecord::Base
+    # attr_accessor :name, :bio
+    
     has_many :reviews
     has_many :products, through: :reviews 
+
+    def favorite_product
+        reviews.order(star_rating: :desc).first.product
+      end
+    
+      def remove_reviews(product)
+        reviews.where(product: product).destroy_all
+      end
+    
 end 
