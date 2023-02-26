2b53820bcf0dc5b0a6de2f1f00153f0f32b96de7
Renee Mumbi <1723renee@gmail.com>
2023-02-26 19:27:23 +0300

    Change attributes from desc to price

--- db/migrate/20230226131910_create_products.rb
+++ db/migrate/20230226131910_create_products.rb
@@ -2,7 +2,7 @@ class CreateProducts < ActiveRecord::Migration[6.1]
   def change
     create_table :products do |t|
       t.string :name
-      t.string :desc
+      t.integer :price
     end
   end
 end
