b8ef5c1dc7268209e1f53f955ab96f8738ab3e4f
Renee Mumbi <1723renee@gmail.com>
2023-02-26 19:23:45 +0300

    Create Review methods for reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,16 @@
 class Review < ActiveRecord::Base
     belongs_to :user
     belongs_to :product
+
+    def print_review
+        puts "Review for #{product.name} by #{user.name}: #{star_rating}. #{comment}"
+      end
+    
+      def user
+        User.find(user_id)
+      end
+    
+      def product
+        Product.find(product_id)
+      end
 end 
